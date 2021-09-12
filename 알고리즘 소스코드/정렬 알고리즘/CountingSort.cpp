97b3b72191c491759c480404b183ef9b618c2bb3
Jaeyoung Lim <limjyjustin@naver.com>
2021-09-12 16:22:24 +0900

    Update CountingSort.cpp

--- 알고리즘 소스코드/정렬 알고리즘/CountingSort.cpp
+++ 알고리즘 소스코드/정렬 알고리즘/CountingSort.cpp
@@ -36,3 +36,39 @@ int main(void) {
 
     CountingSort(v, 5);  // k는 배열값의 상한선
 }
+
+
+// Baekjoon Online Judge 10989 (수 정렬하기 3번 문제)
+// 핵심) 메모리를 최소화하여 구현 (입력을 전부 저장하지 않고도 구현)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void CountingSort(vector<int> &A, int mx) {
+    for(short i = 0; i <= mx; i++) {
+        for(int p = 0; p < A[i]; p++) {
+            cout << i << "\n";
+        }
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    scanf("%d", &N);
+
+    int num; int mx = 0;
+    vector<int> A(10001, 0);
+
+    for(int i = 0; i < N; i++) {
+        cin >> num;
+        A[num]++;
+
+        if(mx < num)
+            mx = num;
+    }
+
+    CountingSort(A, mx);
+}
